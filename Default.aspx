d7bda2869922309938db6f6458a4db3b930f6aef
Kannadasan <KannadasanG@sulekha.net>
2015-08-22 15:26:56 +0530

    First commit

---
+++ Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .grdhead
+        {
+            background-color: #003399 !important;
+            color: White !important;
+            width: 100px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:GridView ID="grdProducts" CssClass="Grid" HeaderStyle-CssClass="header" runat="server" BackColor="White" BorderColor="#3366CC" BorderStyle="None" BorderWidth="1px" CellPadding="4">
+        <FooterStyle BackColor="#99CCCC" ForeColor="#003399" />
+<HeaderStyle CssClass="header" BackColor="#003399" Font-Bold="True" ForeColor="#CCCCFF"></HeaderStyle>
+        <PagerStyle BackColor="#99CCCC" ForeColor="#003399" HorizontalAlign="Left" />
+        <RowStyle BackColor="White" ForeColor="#003399" />
+        <SelectedRowStyle BackColor="#009999" Font-Bold="True" ForeColor="#CCFF99" />
+        <SortedAscendingCellStyle BackColor="#EDF6F6" />
+        <SortedAscendingHeaderStyle BackColor="#0D4AC4" />
+        <SortedDescendingCellStyle BackColor="#D6DFDF" />
+        <SortedDescendingHeaderStyle BackColor="#002876" />
+    </asp:GridView>
+    <br />
+    <asp:Button ID="btnConvertRowstoColumns" runat="server" Text="Rows to Columns" OnClick="btnConvertRowstoColumns_Click"
+        CommandArgument="RowstoColumns" />
+    <asp:Button ID="btnConvertColumnstoRows" runat="server" Text="Columns to Rows" OnClick="btnConvertRowstoColumns_Click"
+        CommandArgument="ColumnstoRows" Visible="false" />
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data;
+using System.Web.UI.WebControls;
+
+public partial class Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            //Creating Data Table of Products With Three Columns
+            DataTable dTable = new DataTable("Products");
+            dTable.Columns.Add("ItemName", typeof(string));
+            dTable.Columns.Add("ItemPrice", typeof(int));
+            dTable.Columns.Add("ItemQuantity", typeof(int));
+
+            //Adding Sample Records
+            dTable.Rows.Add("TeleVision", 14999, 1000);
+            dTable.Rows.Add("16 GB Pendrive", 499, 2000);
+            dTable.Rows.Add("Ear Phones", 99, 500);
+            dTable.Rows.Add("Washing Machine", 16499, 100);
+            dTable.Rows.Add("AC", 18999, 50);
+            dTable.Rows.Add("Water Heater", 19999, 249);
+            dTable.Rows.Add("Water Purifier", 13999, 199);
+            ViewState["dTable"] = dTable;
+            BindGridViewData(dTable, false);
+        }
+    }
+
+    private void BindGridViewData(DataTable dTable, bool IsRotate = false)
+    {
+        grdProducts.ShowHeader = true;
+        if (IsRotate)
+            grdProducts.ShowHeader = false;
+        grdProducts.DataSource = dTable;
+        grdProducts.DataBind();
+        if (IsRotate)
+        {
+            foreach (GridViewRow row in grdProducts.Rows)
+            {
+                row.Cells[0].CssClass = "grdhead";
+            }
+        }
+    }
+    protected void btnConvertRowstoColumns_Click(object sender, EventArgs e)
+    {
+        DataTable dTable = (DataTable)ViewState["dTable"];
+
+        if ((sender as Button).CommandArgument == "RowstoColumns")
+        {
+            DataTable dTable2 = new DataTable();
+            for (int i = 0; i <= dTable.Rows.Count; i++)
+            {
+                dTable2.Columns.Add();
+            }
+            for (int i = 0; i < dTable.Columns.Count; i++)
+            {
+                dTable2.Rows.Add();
+                dTable2.Rows[i][0] = dTable.Columns[i].ColumnName;
+            }
+            for (int i = 0; i < dTable.Columns.Count; i++)
+            {
+                for (int j = 0; j < dTable.Rows.Count; j++)
+                {
+                    dTable2.Rows[i][j + 1] = dTable.Rows[j][i];
+                }
+            }
+            btnConvertRowstoColumns.Visible = false;
+            btnConvertColumnstoRows.Visible = true;
+            BindGridViewData(dTable2, true);
+        }
+        else
+        {
+            btnConvertRowstoColumns.Visible = true;
+            btnConvertColumnstoRows.Visible = false;
+            BindGridViewData(dTable, false);
+        }
+    }
+}
